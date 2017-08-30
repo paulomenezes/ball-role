3181aeb00ae7972d3d050be1c9bf7a965c18c2db
Paulo Menezes <paulo.hgmenezes@gmail.com>
2017-08-30 20:01:43 -0300

    Random walls

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,8 @@ public class PlayerController : MonoBehaviour {
 	public float speed;
 	private Rigidbody rb;
 
+	private float elapsed = 0;
+
 	void Start () {
 		rb = GetComponent<Rigidbody>();
 	}
@@ -16,5 +18,11 @@ public class PlayerController : MonoBehaviour {
 		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, 0.0f);
 
 		rb.AddForce (movement * speed);
+	}
+
+	void Update () {
+		if (gameObject.transform.position.z < -12) {
+			// Vector3.Lerp (gameObject.transform.position, new Vector3 (gameObject.transform.position.x, gameObject.transform.position.y, -12), 0.5f);
+		}
 	}
 }
--- Assets/Scripts/Spawmer.cs
+++ Assets/Scripts/Spawmer.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Spawmer : MonoBehaviour {
 	private Material wallMaterial;
 	private float elapsed = 5f;
+	private float offset = 10;
 
 	void Start () {
 		wallMaterial = Resources.Load("Wall", typeof(Material)) as Material;
@@ -16,18 +17,37 @@ public class Spawmer : MonoBehaviour {
 		if (elapsed >= 10f) {
 			elapsed = 0;
 
-			var cube = GameObject.CreatePrimitive (PrimitiveType.Cube);
-			cube.AddComponent<Rigidbody> ();
-			cube.AddComponent<ScenarioController> ();
-			cube.transform.position = new Vector3 (0, 1f, 0);
-			cube.transform.localScale = new Vector3 (2 * Random.Range(1, 4), 2, 0.5f);
+			var plane = GameObject.Find ("Plane");
+			var planeMesh = plane.GetComponent<MeshFilter> ().mesh;
+			var bounds = planeMesh.bounds;
 
-			Rigidbody rigidBody = cube.GetComponent<Rigidbody> ();
-			rigidBody.isKinematic = true;
-			rigidBody.useGravity = false;
+			float floor = plane.transform.localScale.x * bounds.size.x;
+			float width1 = Random.Range (1, floor - offset);
+			float width2 = Random.Range (1, floor - width1 - offset);
 
-			Renderer renderer = cube.GetComponent<Renderer> ();
-			renderer.material = wallMaterial;
+			offset = floor / 5;
+
+			float x1 = -(floor / 2) + (width1 / 2);
+			float x2 = (floor / 2) - (width2 / 2);
+
+			AddWall (x1, width1);
+			AddWall (x2, width2);
 		}
+	}
+
+	void AddWall(float x, float width) {
+		var cube = GameObject.CreatePrimitive (PrimitiveType.Cube);
+		cube.transform.position = new Vector3 (x, 1f, 0);
+		cube.transform.localScale = new Vector3 (width, 2, 0.5f);
+
+		cube.AddComponent<Rigidbody> ();
+		cube.AddComponent<ScenarioController> ();
+
+		Rigidbody rigidBody = cube.GetComponent<Rigidbody> ();
+		rigidBody.isKinematic = true;
+		rigidBody.useGravity = false;
+
+		Renderer renderer = cube.GetComponent<Renderer> ();
+		renderer.material = wallMaterial;
 	}
 }
